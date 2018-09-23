155d6e02569b6482942cc38362b8794a0dc491d0
Panther59 <utkarsh.chauhan@hotmail.com>
2018-09-23 13:53:36 +0530

    New template integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { LazyLoadModule } from './lazy-load/lazy-load.module';
+import { CoreModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LazyLoadModule,
+    CoreModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/sidemenu/menu-element.ts
@@ -0,0 +1,8 @@
+export const menus = [
+    {
+        'name': 'Dashboard',
+        'icon': 'dashboard',
+        'link': '/auth/dashboard',
+        'open': true,
+    }
+];
